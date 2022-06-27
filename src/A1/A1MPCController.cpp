91d2645fce57c09fa3a1918e767c29d83b1efb28
hosunkang <khsuniv201@gmail.com>
2022-06-28 00:20:27 +0900

    Temp update

--- src/A1/A1MPCController.cpp
+++ src/A1/A1MPCController.cpp
@@ -4,11 +4,19 @@
 
 #include "A1MPCController.h"
 
+#define BIG_NUMBER 5e10
+#define F_MAX 120
+#define MU 0.6
+
+char var_elim[2000];
+char con_elim[2000];
+
 void A1MPCController::doControl() {
     std::cout<<"simTime : "<<getRobot()->getWorldTime()<<std::endl;
     updateState();
     setTrajectory();
     getMetrices();
+    qpSolver();
     computeControlInput();
     setControlInput();
 }
@@ -24,23 +32,26 @@ void A1MPCController::updateState(){
 
     quat_to_euler(quat, q);
 
-    x0 << q[0], q[1], q[2],
+    x0 << 0, 0, 0,
           p[0], p[1], p[2],
           w[0], w[1], w[2],
           v[0], v[1], v[2],
           mGravity;
-    std::cout << x0 << std::endl;
 }
 
 void A1MPCController::setTrajectory() {
     std::cout << "Set trajectory function" << std::endl;
+    double tempXd[13] = {0.f, 0.f, 0.f,
+                         0.f, -1.5, 0.3590,
+                         0.f, 0.f, 0.f,
+                         0.f, 0.f, 0.f,
+                         0.f};
     for(int i = 0; i < mMPCHorizon ; i++)
     {
-        xd << 0.f, 0.f, 0.f,
-              0.f, -1.5, 0.3590,
-              0.f, 0.f, 0.f,
-              0.f, 0.f, 0.f,
-              mGravity;
+        for(int j=0; j<13;j++)
+        {
+            xd(13*i+j,0) = tempXd[j];
+        }
     }
 }
 
@@ -49,7 +60,274 @@ void A1MPCController::getMetrices(){
     Eigen::Matrix<double,13,12> Bc;
 
     ss_mats(Ac, Bc);
+    std::cout << Ac << std::endl;
+    std::cout << Bc << std::endl;
+
     c2qp(Ac,Bc);
+
+    //weights
+    Eigen::Matrix<double, 13,1> weightMat;
+    weightMat << 0.25, 0.25, 10, 2, 2, 20, 0, 0, 0.3, 0.2, 0.2, 0.2, 0.f;
+    L.diagonal() = weightMat.replicate(mMPCHorizon,1);
+
+    H = 2*(Bqp.transpose()*L*Bqp + alpha*K);
+    g = 2*Bqp.transpose()*L*(Aqp*x0 - xd);
+
+    std::cout << Aqp << std::endl;
+    std::cout << Bqp << std::endl;
+    std::cout << H << std::endl;
+    std::cout << g << std::endl;
+
+    int k = 0;
+    for(int i = 0; i < mMPCHorizon; i++)
+    {
+        for(int16_t j = 0; j < 4; j++)
+        {
+            U_b(5*k + 0) = BIG_NUMBER;
+            U_b(5*k + 1) = BIG_NUMBER;
+            U_b(5*k + 2) = BIG_NUMBER;
+            U_b(5*k + 3) = BIG_NUMBER;
+            U_b(5*k + 4) = F_MAX;
+            //U_b(5*k + 4) = update->gait[i*4 + j] * setup->f_max;
+            k++;
+        }
+    }
+
+    //fmat initialize
+    float mu = 1.f/MU;
+    Eigen::Matrix<double,5,3> f_block;
+
+    f_block <<  mu, 0,  1.f,
+            -mu, 0,  1.f,
+            0,  mu, 1.f,
+            0, -mu, 1.f,
+            0,   0, 1.f;
+
+    for(int i = 0; i <  mMPCHorizon*4; i++) //12
+    {
+        fmat.block(i*5,i*3,5,3) = f_block; //Such like diagonal matrix
+    }
+}
+
+int8_t near_zero(float a)
+{
+    return (a < 0.01 && a > -.01) ;
+}
+
+int8_t near_one(float a)
+{
+    return near_zero(a-1);
+}
+
+void A1MPCController::matrix_to_real(qpOASES::real_t* dst, Eigen::Matrix<double,Dynamic,Dynamic> src, int16_t rows, int16_t cols){
+    int32_t a = 0;
+    for(int16_t r = 0; r < rows; r++)
+    {
+        for(int16_t c = 0; c < cols; c++)
+        {
+            dst[a] = src(r,c);
+            a++;
+        }
+    }
+}
+
+void A1MPCController::qpSolver(){
+    qpOASES::real_t* H_qpoases;
+    qpOASES::real_t* g_qpoases;
+    qpOASES::real_t* A_qpoases;
+    qpOASES::real_t* ub_qpoases;
+    qpOASES::real_t* lb_qpoases;
+    qpOASES::real_t* q_soln;
+
+    qpOASES::real_t* H_red;
+    qpOASES::real_t* g_red;
+    qpOASES::real_t* A_red;
+    qpOASES::real_t* lb_red;
+    qpOASES::real_t* ub_red;
+    qpOASES::real_t* q_red;
+
+    H_qpoases = (qpOASES::real_t*)malloc(12*mMPCHorizon*12*mMPCHorizon*sizeof(qpOASES::real_t));
+    g_qpoases = (qpOASES::real_t*)malloc(12*mMPCHorizon*sizeof(qpOASES::real_t));
+    A_qpoases = (qpOASES::real_t*)malloc(12*mMPCHorizon*20*mMPCHorizon*sizeof(qpOASES::real_t));
+    ub_qpoases = (qpOASES::real_t*)malloc(20*mMPCHorizon*sizeof(qpOASES::real_t));
+    lb_qpoases = (qpOASES::real_t*)malloc(20*mMPCHorizon*sizeof(qpOASES::real_t));
+    q_soln = (qpOASES::real_t*)malloc(12*mMPCHorizon*sizeof(qpOASES::real_t));
+
+    H_red = (qpOASES::real_t*)malloc(12*12*mMPCHorizon*mMPCHorizon*sizeof(qpOASES::real_t));
+    g_red = (qpOASES::real_t*)malloc(12*1*mMPCHorizon*sizeof(qpOASES::real_t));
+    A_red = (qpOASES::real_t*)malloc(12*20*mMPCHorizon*mMPCHorizon*sizeof(qpOASES::real_t));
+    lb_red = (qpOASES::real_t*)malloc(20*1*mMPCHorizon*sizeof(qpOASES::real_t));
+    ub_red = (qpOASES::real_t*)malloc(20*1*mMPCHorizon*sizeof(qpOASES::real_t));
+    q_red = (qpOASES::real_t*)malloc(12*mMPCHorizon*sizeof(qpOASES::real_t));
+
+    matrix_to_real(H_qpoases, H, 12*mMPCHorizon, 12*mMPCHorizon);
+    matrix_to_real(g_qpoases, g, 12*mMPCHorizon, 1);
+    matrix_to_real(A_qpoases, fmat, 20*mMPCHorizon, 12*mMPCHorizon);
+    matrix_to_real(ub_qpoases,U_b, 20*mMPCHorizon, 1);
+
+    for(int i =0; i<20*mMPCHorizon; i++)
+        lb_qpoases[i] = 0.f;
+
+    // Set red matrices
+    int16_t num_constraints = 20*mMPCHorizon;
+    int16_t num_variables = 12*mMPCHorizon;
+
+    int new_cons = num_constraints;
+    int new_vars = num_variables;
+
+    //Set the length of metrices depend on variables and constraintss
+    for(int i=0; i<num_constraints; i++)
+        con_elim[i] = 0;
+    for(int i=0; i<num_variables; i++)
+        var_elim[i] = 0;
+
+    // Both of lb, ub is not near the zero, var_elim[5]/con_elim[3] => 1
+    // the other side, var_elim[5]/con_elim[3] => 0
+    for(int i=0; i<num_constraints; i++)
+    {
+        // near the zero (-0.01 ~ 0.01)
+        // lb_qpoases => all zero
+        // ub_qpoases => 0~3: BIG_VALUE ,
+        //            =>  4 : depend on gait[0 or 1]*f_max / if stand it all set f_max.
+        if(!(near_zero(lb_qpoases[i]) && near_zero(ub_qpoases[i])))
+            continue;
+
+        // Both of lb, ub is not near the zero
+        // If gait is 0, the code below is passed
+        double* c_row = &A_qpoases[i*num_variables];
+        for(int j=0; j<num_variables; j++)
+        {
+            if(near_one(c_row[j])) //f_block third column values
+            {
+                //std::cout << i << "\t" << j << "\t" <<c_row[j] << std::endl;
+                new_vars -= 3;
+                new_cons -= 5;
+                int cs = (j*5)/3 -3;
+                var_elim[j-2] = 1;
+                var_elim[j-1] = 1;
+                var_elim[j  ] = 1;
+                con_elim[cs+4] = 1;
+                con_elim[cs+3] = 1;
+                con_elim[cs+2] = 1;
+                con_elim[cs+1] = 1;
+                con_elim[cs  ] = 1;
+            }
+        }
+    }
+
+    int var_idx[new_vars];
+    int con_idx[new_cons];
+    int count = 0;
+
+    // If gait is 1, save the indexes
+    for(int i=0; i<num_variables; i++)
+    {
+        // If gait is 1
+        if(!var_elim[i])
+        {
+            if(!(count<new_vars))
+            {
+                std::cout << "BAD ERROR" << std::endl;
+            }
+            var_idx[count] = i;
+            count++;
+        }
+    }
+    count=0;
+    for(int i=0; i<num_constraints; i++)
+    {
+        // If gait is 1
+        if(!con_elim[i])
+        {
+            if(!(count<new_cons))
+            {
+                std::cout << "BAD ERROR" << std::endl;
+            }
+            con_idx[count] = i;
+            count++;
+        }
+    }
+
+    for(int i=0; i<new_vars;i++)
+    {
+        int old_a = var_idx[i];
+        g_red[i] = g_qpoases[old_a];
+        for(int j=0; j<new_vars; j++)
+        {
+            int old_b = var_idx[j];
+            H_red[i*new_vars+j] = H_qpoases[old_a * num_variables + old_b];
+        }
+    }
+    for(int i=0; i<new_cons;i++)
+    {
+        for(int j=0; j<new_vars; j++)
+        {
+            float cval = A_qpoases[(num_variables*con_idx[i]) + var_idx[j]];
+            A_red[i*new_vars+j] = cval;
+        }
+    }
+
+    for(int i=0; i<new_cons; i++)
+    {
+        int old = con_idx[i];
+        ub_red[i] = ub_qpoases[old];
+        lb_red[i] = lb_qpoases[old];
+    }
+
+    // Solve the problem using qpOASES
+    qpOASES::int_t nWSR = 10000;
+    qpOASES::QProblem problem(new_vars, new_cons);
+    qpOASES::Options op;
+    op.setToMPC();
+    op.printLevel = qpOASES::PL_NONE;
+    problem.setOptions(op);
+    problem.init(H_red, g_red, A_red, NULL, NULL, lb_red, ub_red, nWSR);
+
+    int rval = problem.getPrimalSolution(q_red);
+    if(rval != qpOASES::SUCCESSFUL_RETURN)
+        printf("failed to solve!\n");
+
+    int vc = 0;
+    for(int i = 0; i < num_variables; i++)
+    {
+        if(var_elim[i])
+        {
+            q_soln[i] = 0.0f;
+        }
+        else
+        {
+            q_soln[i] = q_red[vc];
+            vc++;
+        }
+    }
+
+    // [Fx,Fy,Fz]
+    std::cout << "[F values] " << std::endl;
+    for(int leg = 0; leg < 4; leg++)
+    {
+        std::cout << "Leg " << leg+1 << ": ";
+        Eigen::Matrix<double,3,1> f;
+        for(int axis = 0; axis < 3; axis++)
+        {
+            f[axis] = q_soln[leg*3 + axis];
+            std::cout << f[axis] << "\t";
+        }
+        std::cout << std::endl;
+    }
+
+    free(H_qpoases);
+    free(g_qpoases);
+    free(A_qpoases);
+    free(lb_qpoases);
+    free(ub_qpoases);
+    free(q_soln);
+
+    free(H_red);
+    free(g_red);
+    free(A_red);
+    free(lb_red);
+    free(ub_red);
+    free(q_red);
 }
 
 void A1MPCController::computeControlInput() {}
@@ -83,9 +361,9 @@ void A1MPCController::quat_to_euler(Eigen::Matrix<double,4,1>& quat, Eigen::Matr
 {
     //edge case!
     float as = t_min(-2.*(quat[1]*quat[3]-quat[0]*quat[2]),.99999);
-    q(2) = atan2(2.f*(quat[1]*quat[2]+quat[0]*quat[3]),sq(quat[0]) + sq(quat[1]) - sq(quat[2]) - sq(quat[3]));
-    q(1) = asin(as);
     q(0) = atan2(2.f*(quat[2]*quat[3]+quat[0]*quat[1]),sq(quat[0]) - sq(quat[1]) - sq(quat[2]) + sq(quat[3]));
+    q(1) = asin(as);
+    q(2) = atan2(2.f*(quat[1]*quat[2]+quat[0]*quat[3]),sq(quat[0]) + sq(quat[1]) - sq(quat[2]) - sq(quat[3]));
 }
 
 void A1MPCController::ss_mats(Eigen::Matrix<double,13,13>& A, Eigen::Matrix<double,13,12>& B){
@@ -94,8 +372,6 @@ void A1MPCController::ss_mats(Eigen::Matrix<double,13,13>& A, Eigen::Matrix<doub
     A(4,10) = 1.f;
     A(5,11) = 1.f;
     A(11,12) = 1.f;
-
-    B.setZero();
 
     Eigen::Matrix<double,3,3> I_world;
     I_world << 0.01683993, 0, 0,
@@ -151,4 +427,5 @@ void A1MPCController::c2qp(Eigen::Matrix<double,13,13> A, Eigen::Matrix<double,1
         }
     }
 }
+
 //=============================================================
--- src/A1/A1MPCController.h
+++ src/A1/A1MPCController.h
@@ -7,6 +7,10 @@
 
 #include "include/CAMEL/Controller.h"
 #include "A1Robot.h"
+#include <unsupported/Eigen/MatrixFunctions>
+#include <qpOASES.hpp>
+
+using Eigen::Dynamic;
 
 class A1MPCController : public Controller {
 public:
@@ -18,25 +22,29 @@ public:
     double torqueLimit = 20.0;
 
     A1MPCController(Robot *robot, double dT) : Controller(robot){
-        updateState();
         mDT = dT;
+        K.setIdentity();
     }
     void doControl() override;
     void updateState() override;
     void setTrajectory() override;
     void getMetrices();
+    void qpSolver();
     void computeControlInput() override;
     void setControlInput() override;
 
     void quat_to_euler(Eigen::Matrix<double,4,1>& quat, Eigen::Matrix<double,3,1>& q);
     void ss_mats(Eigen::Matrix<double,13,13>& Ac, Eigen::Matrix<double,13,12>& Bc);
     void c2qp(Eigen::Matrix<double,13,13> A, Eigen::Matrix<double,13,12> B);
+    void matrix_to_real(qpOASES::real_t* dst, Eigen::Matrix<double,Dynamic,Dynamic> src, int16_t rows, int16_t cols);
 
 private:
-    double mLumpedMass = 13.f;
+    double mLumpedMass = 11.f;
     double mGravity = -9.81;
-    int mMPCHorizon = 1;
+    int mMPCHorizon = 2;
     double mDT;
+
+    double alpha = 1e-6;
 
     Eigen::Matrix<double, 4, 1> quat;
     Eigen::Matrix<double, 3, 1> p;
@@ -49,6 +57,14 @@ private:
 
     Eigen::MatrixXd Aqp = Eigen::MatrixXd(13*mMPCHorizon, 13);
     Eigen::MatrixXd Bqp = Eigen::MatrixXd(13*mMPCHorizon, 12*mMPCHorizon);
+
+    Eigen::MatrixXd L = Eigen::MatrixXd(13*mMPCHorizon, 13*mMPCHorizon);
+    Eigen::MatrixXd K = Eigen::MatrixXd(12*mMPCHorizon, 12*mMPCHorizon);
+    Eigen::MatrixXd H = Eigen::MatrixXd(12*mMPCHorizon, 12*mMPCHorizon);
+    Eigen::MatrixXd g = Eigen::MatrixXd(12*mMPCHorizon, 1);
+
+    Eigen::MatrixXd U_b = Eigen::MatrixXd(20*mMPCHorizon,1);
+    Eigen::MatrixXd fmat = Eigen::MatrixXd(20*mMPCHorizon,12*mMPCHorizon);
 };
 
 #endif //RAISIM_A1MPCCONTROLLER_H

1b8a44494b6f278420875137de6d042cf77fe9b1
ohDDo <dasdas12002@gmail.com>
2022-07-10 16:36:54 +0900

    cleaned up MIP PDD controller

--- src/MomentumInvertedPendulum/MIPPDController.h
+++ src/MomentumInvertedPendulum/MIPPDController.h
@@ -6,32 +6,20 @@
 #define RAISIM_MIPPDCONTROLLER_H
 
 #include "include/CAMEL/Controller.h"
-//#include <random>
+
+#define FORCE_DURATION 400
+#define MAX_POSITION_NOISE 0.001 //withstand 0.05
+#define MAX_VELOCITY_NOISE 0.01 //withstand 0.6
+#define RANDOM_BOUNDARY 100
 
 class MIPPDController : public Controller{
 public:
-    Eigen::VectorXd torque = Eigen::VectorXd(2);
-    raisim::VecDyn position = raisim::VecDyn(2);
-    raisim::VecDyn velocity = raisim::VecDyn(2);
-    Eigen::VectorXd positionError = Eigen::VectorXd(1);
-    Eigen::VectorXd velocityError = Eigen::VectorXd(1);
-    Eigen::VectorXd desiredJointPosition = Eigen::VectorXd(1);
-    Eigen::VectorXd desiredJointVelocity = Eigen::VectorXd(1);
-
-    raisim::Vec<3> externalForce;
-    raisim::Vec<3> forcePosition;
-    double desiredPosition;
-    double desiredVelocity;
-
-    double PGain;
-    double DGain;
-    double torqueLimit = 3.5;
 
     MIPPDController(Robot *robot) : Controller(robot) {
         setTrajectory();
         setPDGain(7.0, 0.4);
-        torque.setZero();
-        i = 0;
+        mTorque.setZero();
+        mIteration = 0;
     }
 
     void doControl() override;
@@ -40,9 +28,25 @@ public:
     void computeControlInput() override;
     void setControlInput() override;
     void setPDGain(double PGain, double DGain);
+    const Eigen::VectorXd &getTorque() const;
 
 private:
-    int i;
+    raisim::VecDyn mPosition = raisim::VecDyn(2);
+    raisim::VecDyn mVelocity = raisim::VecDyn(2);
+    Eigen::VectorXd mTorque = Eigen::VectorXd(2);
+    Eigen::VectorXd mDesiredJointPosition = Eigen::VectorXd(1);
+    Eigen::VectorXd mDesiredJointVelocity = Eigen::VectorXd(1);
+    raisim::Vec<3> mExternalForce;
+    raisim::Vec<3> mForcePosition;
+    double mDesiredRodPosition;
+    double mDesiredRodVelocity;
+    double mPositionError;
+    double mVelocityError;
+    double mPGain;
+    double mDGain;
+    double mTorqueLimit = 3.5;
+    int mIteration;
+
     void generateExternalForce();
     void addNoise();
 };

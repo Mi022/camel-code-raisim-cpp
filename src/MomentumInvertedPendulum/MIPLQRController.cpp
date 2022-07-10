36e56a827f34a2af29d2ab55d0594fa7778a148c
ohDDo <dasdas12002@gmail.com>
2022-07-10 17:22:57 +0900

    cleaned up LQR PD controller

--- src/MomentumInvertedPendulum/MIPLQRController.cpp
+++ src/MomentumInvertedPendulum/MIPLQRController.cpp
@@ -5,48 +5,45 @@
 //
 
 #include "MIPLQRController.h"
-using namespace std;
 
 void MIPLQRController::setMatrix() {
-    A <<    1.00165, 0.005, 0.0,
+    mA <<    1.00165, 0.005, 0.0,
             0.6606, 1.0, 0.0,
             -0.6606, 0.0, 1.0;
-    B <<    0.00718,
+    mB <<    0.00718,
             -2.872,
             24.442;
 }
 void MIPLQRController::setSNGain(double SN11, double SN22, double SN33) {
-    SN<<    SN11, 0.0, 0.0,
+    mSN<<    SN11, 0.0, 0.0,
             0.0, SN22, 0.0,
             0.0, 0.0, SN33;
 }
 
 void MIPLQRController::setQGain(double Q11, double Q22, double Q33) {
-    Q <<    Q11, 0.0, 0.0,
+    mQ <<    Q11, 0.0, 0.0,
             0.0, Q22, 0.0,
             0.0, 0.0, Q33;
 }
 
 void MIPLQRController::setRGain(double R) {
-    this -> R = R;
+    this -> mR = R;
 }
 
 void MIPLQRController::findS() {
     int iteration = 0;
-    Eigen::Matrix3d snext = SN;
-    double temp = B.transpose() * snext * B + R;
-    S = A.transpose() * (snext - snext * B / temp * B.transpose() * snext) * A + Q;
-    while(!IsSEnough(S, snext)){
+    Eigen::Matrix3d snext = mSN;
+    double temp = mB.transpose() * snext * mB + mR;
+    mS = mA.transpose() * (snext - snext * mB / temp * mB.transpose() * snext) * mA + mQ;
+    while(!IsSEnough(mS, snext)){
          iteration++;
-         cout<<"check: "<<iteration<<endl;
-         if(iteration > 10000)
+         if(iteration > MAXIMUM_ITERATION)
          {
-             SExist = false;
+             mIsSExist = false;
              break;
          }
-         snext = S;
-         S = A.transpose() * (snext - snext * B / (B.transpose() * snext * B + R) * B.transpose() * snext) * A + Q;
-         cout<<"check S :\n"<<S<<endl;
+         snext = mS;
+         mS = mA.transpose() * (snext - snext * mB / (mB.transpose() * snext * mB + mR) * mB.transpose() * snext) * mA + mQ;
     }
 }
 
@@ -54,7 +51,7 @@ bool MIPLQRController::IsSEnough(Eigen::Matrix3d S, Eigen::Matrix3d Snext) {
     bool tempJudge = true;
     for(int r = 0; r<3; r++){
         for(int c = 0; c<3; c++){
-            tempJudge = tempJudge && abs(S(r,c) - Snext(r, c)) < 0.001;
+            tempJudge = tempJudge && abs(S(r,c) - Snext(r, c)) < TOLERANCE;
         }
     }
     if(tempJudge){
@@ -65,31 +62,18 @@ bool MIPLQRController::IsSEnough(Eigen::Matrix3d S, Eigen::Matrix3d Snext) {
     }
 }
 void MIPLQRController::findK() {
-    double temp = B.transpose()*S*B + R;
-    K = B.transpose()*S*A/temp;
-    cout<<"K : "<<K<<endl;
+    double temp = mB.transpose()*mS*mB + mR;
+    mK = mB.transpose()*mS*mA/temp;
 }
 
 void MIPLQRController::generateExternalForce() {
-    raisim::Vec<3> externalForce;
-    raisim::Vec<3> forcePosition;
+    raisim::Vec<3> externalForce = {0.0, 1.9, 0.0};
+    raisim::Vec<3> forcePosition = {0.0, 0.0, 0.08};
 
-    externalForce.setZero();
-    forcePosition.setZero();
-
-    forcePosition[0] = 0.0;
-    forcePosition[1] = 0.0;
-    forcePosition[2] = 0.08;
-
-    externalForce[0] = 0.0;
-    externalForce[1] = 9.6;
-    externalForce[2] = 0.0;
-
-    if(i%400 == 0 || i == 0){
-        std::cout<<"force"<<std::endl;
+    if(mIteration%FORCE_DURATION == 0 || mIteration == 0){
         getRobot()->robot->setExternalForce(1, forcePosition, externalForce);
     }
-    i++;
+    mIteration++;
 }
 
 void MIPLQRController::addNoise() {
@@ -98,21 +82,26 @@ void MIPLQRController::addNoise() {
     // random_device 를 통해 난수 생성 엔진을 초기화 한다.
     std::mt19937 gen(rd());
 
-    // 0 부터 200 까지 균등하게 나타나는 난수열을 생성하기 위해 균등 분포 정의.
-    std::uniform_int_distribution<int> dis(0, 200);
-    double noisePosition = (double(dis(gen)) / 100.0 - 1.0) * 0.001; //0.15
-    double noiseVelocity = (double(dis(gen)) / 100.0 - 1.0) * 0.01; //1.4
-    double noiseMotorVelocity = (double(dis(gen)) / 100.0 - 1.0) * 0.001; //35.5
+    // -100 부터 100 까지 균등하게 나타나는 난수열을 생성하기 위해 균등 분포 정의.
+    std::uniform_int_distribution<int> dis(-RANDOM_BOUNDARY, RANDOM_BOUNDARY);
+    double noisePosition = double(dis(gen)) / RANDOM_BOUNDARY * MAX_POSITION_NOISE; //0.15
+    double noiseVelocity = double(dis(gen)) / RANDOM_BOUNDARY * MAX_VELOCITY_NOISE; //1.4
+    double noiseMotorVelocity = double(dis(gen)) / RANDOM_BOUNDARY * MAX_MOTOR_VELOCITY_NOISE; //35.5
 
-    position[0] += noisePosition;
-    velocity[0] += noiseVelocity;
-    velocity[1] += noiseMotorVelocity;
+    mPosition[0] += noisePosition;
+    mVelocity[0] += noiseVelocity;
+    mVelocity[1] += noiseMotorVelocity;
+
+    mX[0] = mPosition[0];
+    mX[1] = mVelocity[0];
+    mX[2] = mVelocity[1];
 }
 
 void MIPLQRController::doControl() {
     updateState();
+    addNoise();
     generateExternalForce();
-    if(SExist)
+    if(mIsSExist)
     {
         computeControlInput();
         setControlInput();
@@ -120,35 +109,30 @@ void MIPLQRController::doControl() {
 }
 
 void MIPLQRController::updateState() {
-//    for ideal
-    position = getRobot()->robot->getGeneralizedCoordinate();
-    velocity = getRobot()->robot->getGeneralizedVelocity();
-
-//    for adding noise
-    addNoise();
-
-    X[0] = position[0];
-    X[1] = velocity[0];
-    X[2] = velocity[1];
+    mPosition = getRobot()->robot->getGeneralizedCoordinate();
+    mVelocity = getRobot()->robot->getGeneralizedVelocity();
 }
 
 void MIPLQRController::computeControlInput() {
-//    torque[1] = - (K[0]*X[0] + K[1]*X[1] + K[2]*X[2]);
-    torque[1] = -K*X;
-    if(torque[1] > torqueLimit)
+    mTorque[1] = -mK*mX;
+    if(mTorque[1] > mTorqueLimit)
     {
-        torque[1] = torqueLimit;
+        mTorque[1] = mTorqueLimit;
     }
-    else if(torque[1] < -torqueLimit)
+    else if(mTorque[1] < -mTorqueLimit)
     {
-        torque[1] = -torqueLimit;
+        mTorque[1] = -mTorqueLimit;
     }
 }
 
 void MIPLQRController::setControlInput() {
-    getRobot()->robot->setGeneralizedForce(torque);
+    getRobot()->robot->setGeneralizedForce(mTorque);
 }
 
 void MIPLQRController::setTrajectory() {
 
+}
+
+const Eigen::VectorXd &MIPLQRController::getTorque() const {
+    return mTorque;
 }
--- src/MomentumInvertedPendulum/MIPLQRController.h
+++ src/MomentumInvertedPendulum/MIPLQRController.h
@@ -8,29 +8,26 @@
 #include "include/CAMEL/Controller.h"
 #include <Eigen/Core>
 
+#define FORCE_DURATION 400
+#define MAXIMUM_ITERATION 10000
+#define TOLERANCE 0.001
+#define MAX_POSITION_NOISE 0.001 //withstand 0.15
+#define MAX_VELOCITY_NOISE 0.01 //withstand 1.4
+#define MAX_MOTOR_VELOCITY_NOISE 0.001 //withstand 35.5
+#define RANDOM_BOUNDARY 100
+
 class MIPLQRController : public Controller {
 public:
-    Eigen::VectorXd torque = Eigen::VectorXd(2);
-    raisim::VecDyn position = raisim::VecDyn(2);
-    raisim::VecDyn velocity = raisim::VecDyn(2);
-
-
-
-//    raisim::Vec<3> mExternalForce;
-//    raisim::Vec<3> mForcePosition;
-
-    double torqueLimit = 3.5;
-
     MIPLQRController(Robot *robot) : Controller(robot) {
-        i = 0;
+        mIteration = 0;
         setMatrix();
         setSNGain(100.0, 100.0, 100.0);
         setQGain(10.0, 100.0, 1.0);
         setRGain(1.0);
-        torque.setZero();
-        S.setZero();
+        mTorque.setZero();
+        mS.setZero();
         findS();
-        if(SExist) {
+        if(mSExist) {
             findK();
         }
         else{
@@ -43,18 +40,23 @@ public:
     void setTrajectory() override;
     void computeControlInput() override;
     void setControlInput() override;
+    const Eigen::VectorXd &getTorque() const;
 
 private:
-    int i;
-    Eigen::Matrix3d SN;
-    Eigen::Matrix3d Q;
-    double R;
-    Eigen::Matrix3d A;
-    Eigen::Matrix3d S;
-    Eigen::Matrix<double, 3, 1> B;
-    Eigen::Matrix<double, 1, 3> K;
-    Eigen::Matrix<double, 3, 1> X;
-    bool SExist = true;
+    Eigen::VectorXd mTorque = Eigen::VectorXd(2);
+    raisim::VecDyn mPosition = raisim::VecDyn(2);
+    raisim::VecDyn mVelocity = raisim::VecDyn(2);
+    int mIteration;
+    Eigen::Matrix3d mSN;
+    Eigen::Matrix3d mQ;
+    double mR;
+    Eigen::Matrix3d mA;
+    Eigen::Matrix3d mS;
+    Eigen::Matrix<double, 3, 1> mB;
+    Eigen::Matrix<double, 1, 3> mK;
+    Eigen::Matrix<double, 3, 1> mX;
+    bool mIsSExist = true;
+    double mTorqueLimit = 3.5;
 
     void setSNGain(double SN11, double SN22, double SN33);
     void setQGain(double Q11, double Q22, double Q33);

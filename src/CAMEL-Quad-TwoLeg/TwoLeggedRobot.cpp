6289f2f1ad7e20a26923ad4ebfe6d21904cde8ed
ohDDo <dasdas12002@gmail.com>
2022-07-11 17:15:29 +0900

    temporary urdf

--- src/CAMEL-Quad-TwoLeg/TwoLeggedRobot.cpp
+++ src/CAMEL-Quad-TwoLeg/TwoLeggedRobot.cpp
@@ -7,9 +7,16 @@
 void TwoLeggedRobot::initialize() {
     Eigen::VectorXd initialJointPosition(robot->getGeneralizedCoordinateDim());
     initialJointPosition.setZero();
-    initialJointPosition[0] = 0.23;  //prismatic joint
-    initialJointPosition[1] = 60.0 * 3.141592 / 180.0;
-    initialJointPosition[2] = -120.0 * 3.141592 / 180.0;
+    //base position (x,y,z)
+    initialJointPosition[0] = 0.0;  //prismatic joint
+    initialJointPosition[1] = 0.0;
+    initialJointPosition[2] = 1.0;
+
+    //rotation[quaternion]
+    initialJointPosition[3] = 1.0;
+    initialJointPosition[4] = 0.0;
+    initialJointPosition[5] = 0.0;
+    initialJointPosition[6] = 0.0;
     robot->setGeneralizedCoordinate(initialJointPosition);
 }
 

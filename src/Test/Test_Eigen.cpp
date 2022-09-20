0ac6f5217922c1d1f5f1a81f28a49eaa4abd2403
hwayoung <dasdas12002@gmail.com>
2022-09-20 17:40:00 +0900

    added eigen block edit function test

--- src/Test/Test_Eigen.cpp
+++ src/Test/Test_Eigen.cpp
@@ -3,6 +3,8 @@
 //
 
 #include "Eigen/Dense"
+#include <unsupported/Eigen/MatrixFunctions>
+
 #include <iostream>
 
 using namespace std;
@@ -29,7 +31,8 @@ Quaterniond euler2quaternion(Vector3d euler){
         * AngleAxisd(euler[2], Vector3d::UnitZ());
     return q;
 }
-int main(){
+
+void test1(){
     int n;
     MatrixXd m = MatrixXd::Random(5, 3);
     cout << "m =" << endl << m << endl;
@@ -90,4 +93,50 @@ int main(){
     cout<< "qSLERP = " << endl << qSLERP.coeffs()<< endl;
     cout<< "eulerSLERP = "<< endl << eulerSLERP << endl;
     cout<< "90.0*deg2rad = " << 90.0*deg2rad << endl;
+
+}
+
+void test2(){
+    Eigen::Matrix<double,5,5> ABc;
+    Eigen::Matrix<double,4,4> Adt;
+    Eigen::Matrix<double,4,1> Bdt;
+    Eigen::Matrix<double,4,4> Ac;
+    Eigen::Matrix<double,4,1> Bc;
+    double mLumpedMass = 2.009;
+    double mDT = 0.005;
+
+    Ac <<   0, 0, 1, 0,
+            0, 0, 0, 1,
+            52.1064, -39.7950, 0, 0,
+            -43.0680, 53.2449, 0, 0;
+    std::cout<<"Ac : "<<std::endl<<Ac<<std::endl;
+    Bc <<   0,
+            0,
+            2.3899,
+            -3.1977;
+    std::cout<<"Bc : "<<std::endl<<Bc<<std::endl;
+    ABc.setZero();
+
+    ABc.block(0,0,4,4) = Ac;
+    std::cout<<"ABc::blockA : "<<std::endl<<ABc<<std::endl;
+    ABc.block(0,4,4,1) = Bc;
+    std::cout<<"ABc::blockA&B : "<<std::endl<<ABc<<std::endl;
+    ABc = ABc*mDT;
+    ABc = ABc.exp(); // y'=My => y(t)=exp(M)y(0)
+    std::cout<<"ABc::blockA&B : "<<std::endl<<ABc<<std::endl;
+    Adt = ABc.block(0,0,4,4);
+    std::cout<<"Adt : "<<std::endl<<Adt<<std::endl;
+    Bdt = ABc.block(0,4,4,1);
+    std::cout<<"Bdt : "<<std::endl<<Bdt<<std::endl;
+
+}
+
+void test_matrixSize(){
+    Eigen::MatrixXd TestM = Eigen::MatrixXd(3,4);
+    std::cout<<"TestM size: "<<std::endl<< TestM.rows() << ", " << TestM.cols() <<std::endl;
+}
+int main(){
+//    test1();
+//    test2();
+    test_matrixSize();
 }

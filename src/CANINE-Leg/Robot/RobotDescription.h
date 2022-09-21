5bb8e3800f8f6a79d77456f8689c36d74185a08e
ilee <inholee8@pusan.ac.kr>
2022-09-21 13:34:00 +0900

    added robot description for Canine leg operation

---
+++ src/CANINE-Leg/Robot/RobotDescription.h
@@ -0,0 +1,13 @@
+//
+// Created by camel on 22. 9. 21.
+//
+
+#ifndef RAISIM_ROBOTDESCRIPTION_H
+#define RAISIM_ROBOTDESCRIPTION_H
+
+#define HIP_POS_OFFSET      0.8300
+#define KNEE_POS_OFFSET     -3.2021
+#define LUMPED_MASS         2.766
+
+
+#endif //RAISIM_ROBOTDESCRIPTION_H

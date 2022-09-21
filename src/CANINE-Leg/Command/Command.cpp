468603c35e53dbd61051c60ae2bd68f7b0d4d45c
ilee <inholee8@pusan.ac.kr>
2022-09-21 13:33:27 +0900

    added command functions for Canine leg operation

---
+++ src/CANINE-Leg/Command/Command.cpp
@@ -0,0 +1,61 @@
+#include "Command.h"
+
+extern pUI_COMMAND sharedCommand;
+extern pSHM sharedMemory;
+
+void Command::commandFunction()
+{
+    if(sharedMemory->newCommand)
+    {
+        sharedMemory->newCommand = false;
+        int incomingCommand = sharedCommand->userCommand;
+
+        switch(incomingCommand)
+        {
+            case CAN_ON:
+            {
+                mCan->canInit();
+                mCan->readMotorErrorStatus();
+                break;
+            }
+            case VISUAL_ON:
+            {
+                sharedMemory->visualState = STATE_OPEN_RAISIM;
+                break;
+            }
+            case MOTOR_ON:
+            {
+                mCan->turnOnMotor();
+                sharedMemory->controlState = STATE_READY;
+                break;
+            }
+            case MOTOR_OFF:
+            {
+                mCan->turnOffMotor();
+                sharedMemory->controlState = STATE_CONTROL_STOP;
+                break;
+            }
+            case HOME:
+            {
+                sharedMemory->controlState = STATE_HOME_READY;
+                break;
+            }
+            case PD_CMD:
+            {
+                sharedMemory->controlState = STATE_PD_CONTROL;
+                break;
+            }
+            case CUSTOM_1:
+            {
+                sharedMemory->controlState = STATE_READY;
+                break;
+            }
+            case CUSTOM_2:
+            {
+                break;
+            }
+            default:
+                break;
+        }
+    }
+}
---
+++ src/CANINE-Leg/Command/Command.h
@@ -0,0 +1,21 @@
+#ifndef RAISIM_COMMAND_H
+#define RAISIM_COMMAND_H
+
+#include "../Utils/SharedMemory.h"
+#include "../Utils/MotorCAN.h"
+
+class Command {
+public:
+    Command(MotorCAN *can)
+    {
+        mCan = can;
+    }
+    void commandFunction();
+
+private:
+    MotorCAN *mCan;
+    void visualOn();
+};
+
+
+#endif //RAISIM_COMMAND_H

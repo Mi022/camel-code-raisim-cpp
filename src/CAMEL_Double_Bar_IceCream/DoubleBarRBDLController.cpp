42492d25b640973aa2f9d20065d289500346aa93
hwayoung <dasdas12002@gmail.com>
2022-09-22 22:39:09 +0900

    add inverse dynamic controller with RBDL library

---
+++ src/CAMEL_Double_Bar_IceCream/DoubleBarRBDLController.cpp
@@ -0,0 +1,47 @@
+//
+// Created by hwayoung on 22. 9. 22.
+//
+
+#include "DoubleBarRBDLController.h"
+
+void DoubleBarRBDLController::doControl() {
+    setTrajectory();
+    updateState();
+    computeControlInput();
+    setControlInput();
+}
+
+void DoubleBarRBDLController::setTrajectory() {
+    desiredPosition << -30*3.141592/180, -2*30*3.141592/180, -(90-30)*3.141592/180;
+    desiredVelocity.setZero();
+    desiredAccerelation = (desiredVelocity - velocity)/0.005;
+}
+
+void DoubleBarRBDLController::updateState() {
+    position = getRobot()->getQ().e();
+    velocity = getRobot()->getQD().e();
+}
+
+void DoubleBarRBDLController::computeControlInput() {
+    std::cout<<"Tau_Before : "<<std::endl<<torque<<std::endl;
+    std::cout << "desired:" << std::endl<< "Q:" << std::endl<< position << std::endl<< "QDot:" << std::endl<< velocity << std::endl<< "QDDot:" << std::endl<< desiredAccerelation << std::endl;
+    RigidBodyDynamics::InverseDynamics(*model, position, velocity, desiredAccerelation, torque);
+    std::cout<<"Tau_After : "<<std::endl<<torque<<std::endl;
+}
+
+// set computed force(torque) to the robot
+void DoubleBarRBDLController::setControlInput() {
+//    torque[0] = 0;
+    getRobot()->robot->setGeneralizedForce(torque);
+    std::cout<<"Tau : "<<std::endl<<torque<<std::endl;
+}
+
+void DoubleBarRBDLController::getModelFromURDF() {
+    model = new RigidBodyDynamics::Model();
+    std::string rbdlSourcePath = URDF_RSC_DIR;
+    std::string modelFile = rbdlSourcePath;
+    modelFile.append("/camel_double_bar_IceCream.urdf");
+    std::cout<<"hi"<<std::endl;
+    bool modelLoaded = RigidBodyDynamics::Addons::URDFReadFromFile(modelFile.c_str(), model, false);
+    std::cout<<"check: "<<modelLoaded<<std::endl;
+}
---
+++ src/CAMEL_Double_Bar_IceCream/DoubleBarRBDLController.h
@@ -0,0 +1,48 @@
+//
+// Created by hwayoung on 22. 9. 22.
+//
+
+#ifndef RAISIM_DOUBLEBARRBDLCONTROLLER_H
+#define RAISIM_DOUBLEBARRBDLCONTROLLER_H
+
+#include <rbdl/rbdl.h>
+#include <rbdl/rbdl_utils.h>
+#include <rbdl/addons/urdfreader/urdfreader.h>
+
+#include "include/CAMEL/Controller.h"
+#include "DoubleBarRobot.h"
+
+class DoubleBarRBDLController : public Controller {
+public:
+    DoubleBarRBDLController(DoubleBarRobot *robot) : Controller(robot) {
+        getModelFromURDF();
+        torque = Eigen::VectorXd(robot->dim);
+        position = getRobot() -> getQ().e();
+        velocity = getRobot() -> getQD().e();
+
+        desiredPosition = Eigen::VectorXd(robot->dim).setZero();
+        desiredVelocity = Eigen::VectorXd(robot->dim).setZero();
+        desiredAccerelation = Eigen::VectorXd(robot->dim).setZero();
+    }
+
+    Eigen::VectorXd torque;
+    Eigen::VectorXd position;
+    Eigen::VectorXd velocity;
+    Eigen::VectorXd desiredPosition;
+    Eigen::VectorXd desiredVelocity;
+    Eigen::VectorXd desiredAccerelation;
+
+    void doControl() override;
+    void setTrajectory() override;
+    void updateState() override;
+    void computeControlInput() override;
+    void setControlInput() override;
+
+private:
+    void getModelFromURDF();
+    RigidBodyDynamics::Model* model;
+
+};
+
+
+#endif //RAISIM_DOUBLEBARRBDLCONTROLLER_H

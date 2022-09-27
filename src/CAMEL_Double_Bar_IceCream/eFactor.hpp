53b5fedda597faee8d73e2a33132c343d8c1d00a
ohDDo <dasdas12002@gmail.com>
2022-09-27 15:41:12 +0900

    added enum class eFactor

---
+++ src/CAMEL_Double_Bar_IceCream/eFactor.hpp
@@ -0,0 +1,14 @@
+//
+// Created by hwayoung on 22. 9. 26.
+//
+
+#ifndef RAISIM_EFACTOR_HPP
+#define RAISIM_EFACTOR_HPP
+
+enum class eFactor
+{
+    Q,
+    Q_DOT,
+    TAU
+};
+#endif //RAISIM_EFACTOR_HPP
